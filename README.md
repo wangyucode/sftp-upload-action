68bfe19fa993c436bdf8604ec23b4671d9258885
王郁 <wangyu@wycode.cn>
2020-10-27 21:45:51 +0800

    action is ready

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # sftp-upload-action
-sftp upload using sftp-sync-deploy
+
+this is a github action script for upload files to server via SFTP protocol.
+
+it using [sftp-sync-deploy](https://www.npmjs.com/package/sftp-sync-deploy)
+
+
+## input
+
+```
+  host: 'example.com',            // Required.
+  port: 22,                       // Optional, Default to 22.
+  username: 'user',               // Required.
+  password: 'password',           // Optional.
+  localDir: 'dist',               // Required, Absolute or relative to cwd.
+  remoteDir: '/path/to/dest'      // Required, Absolute path only.
+  dryRun: false                   // Optional. Default to false.
+```
+
+## useful link
+
+<https://www.npmjs.com/package/sftp-sync-deploy>
+<https://github.com/dobbydog/sftp-sync-deploy>
---
+++ sftp.js
@@ -0,0 +1,32 @@
+const { deploy } = require('sftp-sync-deploy');
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+console.log(core.getInput('dryRun'));
+
+let config = {
+  host: core.getInput('host'), // Required.
+  port: core.getInput('port'), // Optional, Default to 22.
+  username: core.getInput('username'), // Required.
+  password: core.getInput('password'), // Optional.
+  //  privateKey: '/path/to/key.pem', // Optional.
+  //  passphrase: 'passphrase',       // Optional.
+  //  agent: '/path/to/agent.sock',   // Optional, path to the ssh-agent socket.
+  localDir: core.getInput('localDir'), // Required, Absolute or relative to cwd.
+  remoteDir: core.getInput('remoteDir') // Required, Absolute path only.
+};
+
+let options = {
+  dryRun: core.getInput('dryRun'), // Enable dry-run mode. Default to false
+  excludeMode: 'remove', // Behavior for excluded files ('remove' or 'ignore'), Default to 'remove'.
+  forceUpload: false // Force uploading all files, Default to false(upload only newer files).
+};
+
+deploy(config, options)
+  .then(() => {
+    console.log('sftp upload success!');
+  })
+  .catch(err => {
+    console.error('sftp upload error! ', err);
+    core.setFailed(err.message)
+  });
