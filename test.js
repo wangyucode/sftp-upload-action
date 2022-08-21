589050a3db90efc53addbd9674ce1b164b3a1005
yu.wang <wangyu@wycode.cn>
2022-08-21 14:51:02 +0800

    fix issue 8

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 new Deployer({
     host: '192.168.0.99',
     privateKey: fs.readFileSync('./id_rsa'),
-    localDir: './',
+    localDir: './test',
     remoteDir: '/root/test/'
 }, { exclude: ['node_modules'], dryRun: false })
     .sync();
