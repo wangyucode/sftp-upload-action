da2dd7a88adb15169b3217116383b22976cb7ad5
Benjamin Svobodny <benjamin.svobodny@gmail.com>
2024-11-11 16:32:14 -0500

    fix: use 4 spaces

--- deployer.test.js
+++ deployer.test.js
@@ -1,31 +1,31 @@
 const { Deployer } = require('./lib/deployer');
 
 const excludeOption =
-  '.editorconfig,.env.example,.eslintrc.json,.git*,**/.git*,.npm*,.nvmrc,.prettier*,artisan,composer*,package*,phpunit.xml,tsconfig.json,vite.config.js,.vscode/,resources/css/,resources/js/,tests/,database/,scripts/,node_modules/,.DS_Store';
+    '.editorconfig,.env.example,.eslintrc.json,.git*,**/.git*,.npm*,.nvmrc,.prettier*,artisan,composer*,package*,phpunit.xml,tsconfig.json,vite.config.js,.vscode/,resources/css/,resources/js/,tests/,database/,scripts/,node_modules/,.DS_Store';
 
 const deploy = new Deployer(
-  {
-    host: '192.168.0.99',
-    compress: true,
-    localDir: './test',
-    remoteDir: '/root/test/',
-  },
-  {
-    exclude: excludeOption.split(','),
-    dryRun: false,
-  }
+    {
+        host: '192.168.0.99',
+        compress: true,
+        localDir: './test',
+        remoteDir: '/root/test/',
+    },
+    {
+        exclude: excludeOption.split(','),
+        dryRun: false,
+    }
 );
 
 describe('Deployer', () => {
-  test('isIgnoreFile', () => {
-    expect(deploy.isIgnoreFile('.npmrc')).toBe(true);
-    expect(deploy.isIgnoreFile('.npmignore')).toBe(true);
-    expect(deploy.isIgnoreFile('.github/')).toBe(true);
-    expect(deploy.isIgnoreFile('.gitignore')).toBe(true);
-    expect(deploy.isIgnoreFile('.gitattributes')).toBe(true);
-    expect(deploy.isIgnoreFile('resources/css/')).toBe(true);
-    expect(deploy.isIgnoreFile('resources/views/')).toBe(false);
-    expect(deploy.isIgnoreFile('node_modules/')).toBe(true);
-    expect(deploy.isIgnoreFile('storage/app/public/.gitignore')).toBe(true);
-  });
+    test('isIgnoreFile', () => {
+        expect(deploy.isIgnoreFile('.npmrc')).toBe(true);
+        expect(deploy.isIgnoreFile('.npmignore')).toBe(true);
+        expect(deploy.isIgnoreFile('.github/')).toBe(true);
+        expect(deploy.isIgnoreFile('.gitignore')).toBe(true);
+        expect(deploy.isIgnoreFile('.gitattributes')).toBe(true);
+        expect(deploy.isIgnoreFile('resources/css/')).toBe(true);
+        expect(deploy.isIgnoreFile('resources/views/')).toBe(false);
+        expect(deploy.isIgnoreFile('node_modules/')).toBe(true);
+        expect(deploy.isIgnoreFile('storage/app/public/.gitignore')).toBe(true);
+    });
 });
